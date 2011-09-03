9749d0fd624db99ae5b01172e8c5e96edf547b16
Sylvester Keil <sylvester@keil.or.at>
2011-09-04 00:38:21 +0200

    improved normalizers

--- lib/anystyle/parser/normalizer.rb
+++ lib/anystyle/parser/normalizer.rb
@@ -16,8 +16,11 @@ module Anystyle
 
 			# Default normalizer. Strips punctuation.
 			def normalize(key, hash)
-				return hash if hash[key].nil?
-				hash[key].gsub!(/^\W+|\W+$/, '')
+				token, *dangling =  hash[key]
+				unmatched(key, hash, dangling) unless dangling.empty?
+
+				token.gsub!(/^\W+|\W+$/, '')
+				hash[key] = token
 				hash
 			rescue => e
 				warn e.message
@@ -25,12 +28,14 @@ module Anystyle
 			end
 
 			def normalize_author(hash)
-				authors = hash[:author]
+				authors, *dangling = hash[:author]
+				unmatched(:author, hash, dangling) unless dangling.empty?
 				
 				if authors =~ /\W*[Ee]d(s|itors)?\W*$/ && !hash.has_key?(:editor)
 					hash[:editor] = hash.delete(:author)
 					normalize_editor(hash)
 				else
+		      hash[:etal] = true if !!authors.sub!(/\bet\.?\s*al.*$/i, '')
 					authors.gsub!(/^\W+|\W+$/, '')
 					hash[:author] = normalize_names(authors)
 				end
@@ -42,7 +47,13 @@ module Anystyle
 			end
 			
 	    def normalize_editor(hash)
-	      editors = hash[:editor]
+	      editors, edition = hash[:editor]
+	
+				unless edition.nil?
+					if edition =~ /(\d+)/
+						hash[:edition] = $1.to_i
+					end
+				end
 	
 				editors.gsub!(/^\W+|\W+$/, '')
 				editors.gsub!(/^in\s+/i, '')
@@ -70,10 +81,41 @@ module Anystyle
 			end
 			
 			def normalize_names(names)
-				names
+				names = tokenize_names(names).map do |name|
+					name.strip!
+					name.gsub!(/\b([[:upper:]])(\W|$)/) { [$1, $2 == ?. ? nil : ?., $2].compact.join }
+					name
+				end
+				names.join(' and ')
 			rescue => e
 				warn e.message
 				hash
+			end
+			
+			def tokenize_names(names)
+				s, n, ns, cc = StringScanner.new(names), '', [], 0
+				until s.eos?
+					case
+					when s.scan(/\s+/)
+						n << ' '
+					when s.scan(/and\b|&/)
+						ns << n
+						n, cc = '', 0
+					when s.scan(/,?\s*(jr|sr|ph\.?d|m\.?d|esq)\.?/i)
+						n << s.matched
+					when s.scan(/,/)
+						if cc > 0
+							ns << n
+							n, cc = '', 0							
+						else
+							n << s.matched
+							cc += 1
+						end
+					when s.scan(/\w+/), s.scan(/./)
+						n << s.matched
+					end
+				end
+				ns << n		
 			end
 			
 			def normalize_title(hash)
@@ -83,8 +125,10 @@ module Anystyle
 					hash[:container] = container
 					normalize(:container, hash)
 				end
-				#TODO edition in container also
-				title.gsub!(/[\.,:;]+$/, '')
+
+				extract_edition(title, hash)
+				
+				title.gsub!(/[\.,:;\s]+$/, '')
 				hash[:title] = title
 				
 				hash
@@ -93,12 +137,21 @@ module Anystyle
 				hash
 			end
 			
-			def normalize_booktitle(hash)
-				normalize(:booktitle, hash)
-				
-				booktitle = hash[:booktitle]
-				booktitle.gsub!(/^in\s*/i, '')
+			def extract_edition(token, hash)
+				if token.gsub!(/\W*(\d+)(?:st|nd|rd|th)?\s*ed(?:ition|\.)?\W*/i, '')
+					hash[:edition] = $1.to_i
+				end				
+			end
 			
+			def normalize_booktitle(hash)
+				booktitle, *dangling = hash[:booktitle]
+				unmatched(:booktitle, hash, dangling) unless dangling.empty?
+				
+				booktitle.gsub!(/^in\s*/i, '')
+
+				extract_edition(booktitle, hash)
+
+				title.gsub!(/[\.,:;\s]+$/, '')			
 				hash[:booktitle] = booktitle
 				
 				hash
@@ -108,7 +161,8 @@ module Anystyle
 			end
 			
 			def normalize_container(hash)
-				container = hash[:container]
+				container, *dangling = hash[:container]
+				unmatched(:container, hash, dangling) unless dangling.empty?
 				
 				case container
 				when /dissertation abstracts/i
@@ -125,7 +179,10 @@ module Anystyle
 			end
 			
 			def normalize_date(hash)
-				case date = hash[:date]
+				date, *dangling = hash[:date]
+				unmatched(:date, hash, dangling) unless dangling.empty?
+				
+				case date
 				when /(\d{4})/
 					hash[:year] = $1.to_i
 					hash.delete(:date)
@@ -138,7 +195,10 @@ module Anystyle
 			end
 
 			def normalize_volume(hash)
-				case volume = hash[:volume]
+				volume, *dangling = hash[:volume]
+				unmatched(:volume, hash, dangling) unless dangling.empty?
+				
+				case volume
 				when /\D*(\d+)\D+(\d+[\s&-]+\d+)/
 					hash[:volume], hash[:number] = $1.to_i, $2
 				when /\D*(\d+)\D+(\d+)/
@@ -154,8 +214,11 @@ module Anystyle
 			end
 
 			def normalize_pages(hash)
+				pages, *dangling = hash[:pages]
+				unmatched(:pages, hash, dangling) unless dangling.empty?
+				
 				# "volume.issue (year):pp"
-				case hash[:pages]
+				case pages
 				when /(\d+) (?: \.(\d+))? (?: \( (\d{4}) \))? : (\d.*)/x
 					hash[:volume] = $1.to_i
 					hash[:number] = $2.to_i unless $2.nil?
@@ -176,7 +239,12 @@ module Anystyle
 				hash
 			end
 
-
+			private
+			
+			def unmatched(label, hash, tokens)
+				hash["unmatched-#{label}"] = tokens.join(' ')
+			end
+			
 		end
 
 	end
