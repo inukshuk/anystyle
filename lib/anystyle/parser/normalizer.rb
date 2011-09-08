25b82ea7c6af908d8529febed493718315b1d48f
Sylvester Keil <sylvester@keil.or.at>
2011-09-08 12:44:47 +0200

    improved volume normalizing (extract pages)

--- lib/anystyle/parser/normalizer.rb
+++ lib/anystyle/parser/normalizer.rb
@@ -288,21 +288,26 @@ module Anystyle
 				volume, *dangling = hash[:volume]
 				unmatched(:volume, hash, dangling) unless dangling.empty?
 				
-				case volume
-				when /\D*(\d+)\D+(\d+[\s&-]+\d+)/
-					hash[:volume], hash[:number] = $1.to_i, $2
-				when /(\d+)?\D+no\.\s*(\d+\D+\d+)/
-					hash[:volume] = $1.to_i unless $1.nil?
-					hash[:number] = $2
-				when /(\d+)?\D+no\.\s*(\d+)/
-					hash[:volume] = $1.to_i unless $1.nil?
-					hash[:number] = $2.to_i
-				when /\D*(\d+)\D+(\d+)/
-					hash[:volume], hash[:number] = $1.to_i, $2.to_i
-				when /(\d+)/
-					hash[:volume] = $1.to_i
+				if !hash.has_key?(:pages) && volume =~ /\D*(\d+):(\d+(?:[–-]+)\d+)/
+					hash[:volume], hash[:pages] = $1.to_i, $2
+					hash = normalize_pages(hash)				
+				else
+					case volume
+					when /\D*(\d+)\D+(\d+[\s&-]+\d+)/
+						hash[:volume], hash[:number] = $1.to_i, $2
+					when /(\d+)?\D+no\.\s*(\d+\D+\d+)/
+						hash[:volume] = $1.to_i unless $1.nil?
+						hash[:number] = $2
+					when /(\d+)?\D+no\.\s*(\d+)/
+						hash[:volume] = $1.to_i unless $1.nil?
+						hash[:number] = $2.to_i
+					when /\D*(\d+)\D+(\d+)/
+						hash[:volume], hash[:number] = $1.to_i, $2.to_i
+					when /(\d+)/
+						hash[:volume] = $1.to_i
+					end
 				end
-
+				
 				hash
 			rescue => e
 				warn e.message
