dbf199996d99e7646f4e5b4dbb1622fe83578205
Sylvester Keil <sylvester@keil.or.at>
2014-07-10 12:23:30 +0200

    add authority bibtex variants
    see #8

--- lib/anystyle/parser/parser.rb
+++ lib/anystyle/parser/parser.rb
@@ -262,6 +262,14 @@ module Anystyle
           hash[:address] = hash.delete :location if hash.key?(:location)
           hash[:urldate] = hash.delete :accessed if hash.key?(:accessed)
 
+          if hash.key?(:authority)
+            if [:techreport,:thesis].include?(hash[:type])
+              hash[:institution] = hash.delete :authority
+            else
+              hash[:organization] = hash.delete :authority
+            end
+          end
+
           b << BibTeX::Entry.new(hash)
         end
         b
