69a5f6e9c17fa29cb4c39ba988ef44c913bce5fa
Sylvester Keil <sylvester@keil.or.at>
2017-11-28 13:48:01 +0100

    Move number specs

---
+++ spec/anystyle/feature/number_spec.rb
@@ -0,0 +1,27 @@
+module Anystyle
+  describe "Number Feature" do
+
+    require 'anystyle/feature'
+    require 'anystyle/feature/number'
+
+    let(:f) { Feature::Number.new }
+
+    %w{ (1992) 1992 2011 1776 }.each do |year|
+      it "elicits :year from #{year.inspect}" do
+        expect(f.elicit(year)).to eq(:year)
+      end
+    end
+
+    %w{ (1) (12) (123) }.each do |year|
+      it "elicits :year from #{year.inspect}" do
+        expect(f.elicit(year)).to eq(:numeric)
+      end
+    end
+
+    ['pp', 'pp.', '23-4', '6124--19', '48 - 9', '19–27'].each do |page|
+      it "elicits :page from #{page.inspect}" do
+        expect(f.elicit(page)).to eq(:page)
+      end
+    end
+  end
+end
