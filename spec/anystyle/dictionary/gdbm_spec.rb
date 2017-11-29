b1c7295f311fce6b326f9eabe0d38370a1b377ac
Sylvester Keil <sylvester@keil.or.at>
2017-11-29 22:26:22 +0100

    Add GDBM adapter

---
+++ spec/anystyle/dictionary/gdbm_spec.rb
@@ -0,0 +1,30 @@
+require 'fileutils'
+
+module AnyStyle
+  describe "GDBM Dictionary Adapter" do
+    let(:tmpdir) { Dir.mktmpdir }
+
+    let(:dict) {
+      Dictionary.create({
+        adapter: :gdbm,
+        path: File.join(tmpdir, 'test.db')
+      })
+    }
+
+    after(:each) {
+      dict.close
+      FileUtils.remove_entry tmpdir
+    }
+
+    it "is a Dictionary" do
+      expect(dict).to be_a(Dictionary)
+      expect(dict).to be_instance_of(Dictionary::GDBM)
+    end
+
+    it "can be opened" do
+      expect(-> { dict.open }).to change { dict.open? }
+      expect(File.exists?(dict.options[:path])).to be true
+    end
+  end
+end
+
