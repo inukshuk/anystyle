219c8b19cd073578991f056a6baa2fb03fea2dcc
Sylvester Keil <sylvester@keil.or.at>
2018-03-28 14:47:42 +0200

    Combine footer/header as meta label
    Add and tweak finder features

--- lib/anystyle/feature/words.rb
+++ lib/anystyle/feature/words.rb
@@ -3,21 +3,53 @@ module AnyStyle
     class Words < Feature
       attr_reader :dictionary
 
+      TITLE_WORDS = %w{
+        abstract
+        acknowledgements
+        appendix
+        bibliography
+        chapter
+        cited
+        contents
+        figures
+        introduction
+        references
+        section
+        tables
+        works
+      }
+
       def initialize(dictionary:, **opts)
         super(**opts)
         @dictionary = dictionary
       end
 
       def observe(token, **opts)
-        words = token.scan(/[\p{L}\p{N}_-]+/).map { |word| canonize word }
+        words = token.scan(/\S+/).map { |word| canonize word }.reject(&:empty?)
         numbers = token.scan(/\d+(\.\d+)?/)
+        title = words.count { |word| TITLE_WORDS.include?(word) }
         counts = dictionary.tag_counts(words)
 
         [
           words.length,
+          classify(words[0]),
           numbers.length,
+          ratio(title, words.length),
           *counts.map { |cnt| ratio(cnt, words.length) }
         ]
+      end
+
+      def classify(word)
+        case word
+        when /^(\d+|[vx]?iii?|i?[vx]|)$/i
+          :number
+        when /\d/
+          :numeric
+        when nil
+          :none
+        else
+          :alpha
+        end
       end
     end
   end
