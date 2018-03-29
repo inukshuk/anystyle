514b0df752b082da41e3e5f1ca163d6f19cc0241
Sylvester Keil <sylvester@keil.or.at>
2018-03-29 12:25:57 +0200

    Update parser specs

--- spec/anystyle/parser_spec.rb
+++ spec/anystyle/parser_spec.rb
@@ -10,26 +10,6 @@ module AnyStyle
           'world! world Ll P w wo ! d! lower none T F T T none last other none weak F'
         ]])
       end
-
-      context 'when marking the input as being tagged' do
-        let(:input) {
-          %{<author>A. Cau, R. Kuiper, and W.-P. de Roever.</author><title>Formalising Dijkstra's development strategy within Stark's formalism. </title> <editor> In C. B. Jones, R. C. Shaw, and T. Denvir, editors, </editor> <container-title> Proc. 5th. BCS-FACS Refinement Workshop, </container-title> <date> 1992. </date>}
-        }
-
-        pending 'returns an array of expanded and labelled token sequences for a tagged string' do
-          expect(subject.prepare(input, tagged: true)[0].map(&:label)).to eq(
-            %w{ author author author author author author author author title title title title title title title editor editor editor editor editor editor editor editor editor editor editor container-title container-title container-title container-title container-title date }
-          )
-        end
-
-        pending 'returns an array of expanded and labelled :unknown token sequences for an untagged input' do
-          expect(subject.prepare('hello, world!', tagged: true)[0].map(&:label)).to eq([nil, nil])
-        end
-
-        #pending 'converts xml entitites' do
-        #  expect(subject.prepare("<note>&gt;&gt; &amp; foo</note>", true)[0].map { |t| t[/\S+/] }).to eq(%w{ >> & foo })
-        #end
-      end
     end
 
     describe "#label" do
@@ -92,7 +72,6 @@ module AnyStyle
 
       it 'returns tagged dataset for wapiti format"' do
         dataset = subject.parse(perec, format: :wapiti)
-
         expect(dataset).to be_a(Wapiti::Dataset)
         expect(dataset[0][0].to_a(expanded: false)).to eq(['Perec,', 'author'])
       end
@@ -117,31 +96,26 @@ module AnyStyle
       describe "a pristine model" do
         before(:each) { subject.train '', truncate: true }
 
-        pending 'recognizes trained references' do
-          subject.learn Wapiti::Dataset.new dps[0, 1]
-          expect(subject.parse(dps[0].to_s, format: :wapiti)[0]).to eq(dps[0])
+        it 'recognizes trained references' do
+          subject.learn dps[0]
+          expect(subject.label(dps[0])[0]).to eq(dps[0])
         end
 
-        pending 'recognizes trained references when learnt in one go' do
+        it 'recognizes trained references when learnt in one go' do
           subject.learn dps
-
-          dps.each do |d|
-            expect(subject.parse(strip_tags(d), :tags)[0]).to eq(d)
+          dps.each do |seq|
+            expect(subject.label(seq)[0]).to eq(seq)
           end
         end
 
         pending 'recognizes trained references when learnt separately' do
-          dps.each do |d|
-            subject.learn d
+          dps.each do |seq|
+            subject.learn seq
           end
-
-          dps.each do |d|
-            expect(subject.parse(strip_tags(d), :tags)[0]).to eq(d)
+          dps.each do |seq|
+            expect(subject.label(seq)[0]).to eq(seq)
           end
         end
-      end
-
-      describe "the default model" do
       end
     end
   end
