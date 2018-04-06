20a6c1fa1955d12d9af57b26eddbd3343645468c
Sylvester Keil <sylvester@keil.or.at>
2018-04-06 12:34:41 +0200

    Tweak finder model
    Improve training data

--- lib/anystyle/feature/ref.rb
+++ lib/anystyle/feature/ref.rb
@@ -6,7 +6,8 @@ module AnyStyle
           symbolize(count(token, /\b(1\d|20)\d\d\b/)),
           symbolize(count(token, /(\d[\(:;]\d)|(\d\s*[—–-]+\s*\d)|\bpp?\.|\bvols?\./i)),
           symbolize(count(token, /\b\p{Lu}\./)),
-          symbolize(count(token, /\b(eds?\.|edited by|editors?|hg|hrsg|et al)\b/i))
+          symbolize(count(token, /\b(eds?\.|edited by|editors?|hg|hrsg|et al)\b/i)),
+          token =~ /^\s*\[\d+\]/ ? 'T' : 'F'
         ]
       end
 
