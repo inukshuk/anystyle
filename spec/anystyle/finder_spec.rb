30b2859bb5d274f6e2d8d1f5e6c62d827c368691
Sylvester Keil <sylvester@keil.or.at>
2018-04-06 12:38:32 +0200

    Fix expectation

--- spec/anystyle/finder_spec.rb
+++ spec/anystyle/finder_spec.rb
@@ -16,16 +16,16 @@ module AnyStyle
         f.prepare(doc)
 
         expect(doc[0].observations.map(&:to_s)).to eq(%w{
-          10 13 10 8 1 1 2 Lu Lu 2 numeric 1 0 0 0 0 0 F = - - - - first only
+          10 13 10 8 1 1 2 Lu Lu 2 numeric 1 0 0 0 0 0 F = - - - - F first only
         })
         expect(doc[1].observations.map(&:to_s)).to eq(%w{
-          0 0 0 0 0 0 0 none none 0 none 0 0 0 0 0 0 F = - - - - 3 only
+          0 0 0 0 0 0 0 none none 0 none 0 0 0 0 0 0 F = - - - - F 3 only
         })
         expect(doc[2].observations.map(&:to_s)).to eq(%w{
-          0 0 0 0 0 0 0 none none 0 none 0 0 0 0 0 0 F = - - - - 5 only
+          0 0 0 0 0 0 0 none none 0 none 0 0 0 0 0 0 F = - - - - F 5 only
         })
         expect(doc[3].observations.map(&:to_s)).to eq(%w{
-          42 58 2 7 1 1 10 Lu P 7 alpha 1 0 4 1 3 4 F = + - - - last only
+          42 58 2 7 1 1 10 Lu P 7 alpha 1 0 4 1 3 4 F = + - - - F last only
         })
       end
     end
