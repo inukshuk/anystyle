ecfd320268ddaf5485d6552f0d81db04f3cd2d6d
Sylvester Keil <sylvester@keil.or.at>
2018-04-10 15:23:38 +0200

    Add spacers, average and median word length features

--- lib/anystyle/feature/words.rb
+++ lib/anystyle/feature/words.rb
@@ -26,12 +26,26 @@ module AnyStyle
 
       def observe(token, **opts)
         words = token.scan(/\S+/).map { |word| canonize word }.reject(&:empty?)
+        spacers = token.scan(/\S\s\s+\S/)
         numbers = token.scan(/\d+(\.\d+)?/)
         title = words.count { |word| TITLE_WORDS.include?(word) }
         counts = dictionary.tag_counts(words)
 
+        if words.length > 0
+          len = words.map(&:length).sort
+          avg = len.reduce(0, :+) / len.length
+          med = len.length.even? ?
+            len[(len.length - 1) / 2, 2].reduce(0, :+) / 2 :
+            len[len.length / 2]
+        else
+          avg, med = 0, 0
+        end
+
         [
           words.length,
+          avg,
+          med,
+          spacers.length,
           classify(words[0]),
           numbers.length,
           ratio(title, words.length),
--- spec/anystyle/finder_spec.rb
+++ spec/anystyle/finder_spec.rb
@@ -4,10 +4,10 @@ module AnyStyle
     let(:f) { Finder.new }
     let(:doc) {
       Document.parse <<~EOS
-        A1. REFERENES
+        A1.  REFERENCES
            \t\r\t
 
-        Matsumoto (2006) Ruby: The Programming Language. O'Reilly.
+          Matsumoto (2006) Ruby: The Programming Language. O'Reilly.
       EOS
     }
 
@@ -16,16 +16,16 @@ module AnyStyle
         f.prepare(doc)
 
         expect(doc[0].observations.map(&:to_s)).to eq(%w{
-          10 13 10 8 1 1 2 Lu Lu 2 numeric 1 0 0 0 0 0 F = - - - - F first only
+          11 15 10 7 1 1 3 Lu Lu 2 6 6 1 numeric 1 5 0 0 0 5 F = - - - - F first only
         })
         expect(doc[1].observations.map(&:to_s)).to eq(%w{
-          0 0 0 0 0 0 0 none none 0 none 0 0 0 0 0 0 F = - - - - F 3 only
+          0 0 0 0 0 0 0 none none 0 0 0 0 none 0 0 0 0 0 0 F = - - - - F 3 only
         })
         expect(doc[2].observations.map(&:to_s)).to eq(%w{
-          0 0 0 0 0 0 0 none none 0 none 0 0 0 0 0 0 F = - - - - F 5 only
+          0 0 0 0 0 0 0 none none 0 0 0 0 none 0 0 0 0 0 0 F = - - - - F 5 only
         })
         expect(doc[3].observations.map(&:to_s)).to eq(%w{
-          42 58 2 7 1 1 10 Lu P 7 alpha 1 0 4 1 3 4 F = + - - - F last only
+          42 60 2 7 1 1 10 Lu P 7 6 7 0 alpha 1 0 4 1 3 4 T + + - - - F last only
         })
       end
     end
