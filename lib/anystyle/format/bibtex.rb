8529e2eba12cd31440bc1a3394551c6c9cc33093
Sylvester Keil <sylvester@keil.or.at>
2018-05-28 16:27:54 +0200

    Update BibTeX export

--- lib/anystyle/format/bibtex.rb
+++ lib/anystyle/format/bibtex.rb
@@ -1,28 +1,70 @@
 module AnyStyle
   module Format
     module BibTeX
+      TYPES = {
+        'article-journal' => 'article',
+        'chapter' => 'incollection',
+        'manuscript' => 'unpublished',
+        'paper-conference' => 'inproceedings',
+        'report' => 'techreport'
+      }
+
       def format_bibtex(dataset)
         require 'bibtex'
 
-        b = BibTeX::Bibliography.new
+        b = ::BibTeX::Bibliography.new
         format_hash(dataset).each do |hash|
-          hash[:bibtex_type] = hash.delete(:type) || 'misc'
+          flatten_values hash, { skip: [:author, :editor, :translator] }
 
-          hash[:type] = hash.delete :genre if hash.key?(:genre)
-          hash[:address] = hash.delete :location if hash.key?(:location)
-          hash[:urldate] = hash.delete :accessed if hash.key?(:accessed)
-
-          if hash.key?(:authority)
-            if [:techreport,:thesis].include?(hash[:bibtex_type])
-              hash[:institution] = hash.delete :authority
-            else
-              hash[:organization] = hash.delete :authority
-            end
+          if hash.key?(:type)
+            hash[:bibtex_type] = TYPES[hash[:type]] || hash[:type]
+            hash.delete :type
+          else
+            hash[:bibtex_type] = 'misc'
           end
 
-          b << BibTeX::Entry.new(hash)
+          case hash[:bibtex_type]
+          when 'article'
+            rename_value hash, :'container-title', :journal
+            rename_value hash, :issue, :number
+          when 'techreport'
+            rename_value hash, :publisher, :institution
+          when 'thesis'
+            rename_value hash, :publisher, :school
+          end
+
+          names_to_bibtex hash, :author
+          names_to_bibtex hash, :editor
+          names_to_bibtex hash, :translator
+
+          rename_value hash, :'collection-title', :series
+          rename_value hash, :'container-title', :booktitle
+          rename_value hash, :accessed, :urldate
+          rename_value hash, :genre, :type
+          rename_value hash, :location, :address
+
+          b << ::BibTeX::Entry.new(hash)
         end
         b
+      end
+
+      def names_to_bibtex(hash, role)
+        if hash.key?(role)
+          hash[role] = hash[role].map { |name|
+            case
+            when name.key?(:literal)
+              name[:literal]
+            when name.key?(:family) && name.key(:given)
+              "#{name[:family]}, #{name[:given]}"
+            when name.key?(:family)
+              name[:family]
+            when name.key?(:given)
+              name[:given]
+            else
+              nil
+            end
+          }.compact.join(' and ')
+        end
       end
     end
   end
---
+++ spec/anystyle/format/bibtex_spec.rb
@@ -0,0 +1,20 @@
+module AnyStyle
+  describe Format::BibTeX do
+    let(:ap) { Parser.new }
+
+    let(:refs) {[
+      'Derrida, J. (1967). L’écriture et la différence (1 éd.). Paris: Éditions du Seuil.'
+    ]}
+
+    it 'Converts parse results to BibTeX' do
+      bib = ap.parse(refs, format: 'bibtex')
+      expect(bib).to be_a(::BibTeX::Bibliography)
+      expect(bib.size).to eq(refs.length)
+
+      expect(bib[0].type).to eq(:book)
+      expect(bib[0].author[0].family).to eq('Derrida')
+      expect(bib[0].title).to eq('L’écriture et la différence')
+      expect(bib[0].edition).to eq('1')
+    end
+  end
+end
