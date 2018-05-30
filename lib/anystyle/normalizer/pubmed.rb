cd9c908d9ec08649138191c4b1c99d1037b80e12
Sylvester Keil <sylvester@keil.or.at>
2018-05-30 16:10:22 +0200

    Fix pubmed normalizer

--- lib/anystyle/normalizer/pubmed.rb
+++ lib/anystyle/normalizer/pubmed.rb
@@ -6,10 +6,10 @@ module AnyStyle
       def normalize(item, **opts)
         each_value(item) do |_, value|
           if (value =~ /PMID:?\s*(\d+)/)
-            append :pmid, $1
+            append item, :pmid, $1
           end
           if (value =~ /PMC(\d+)/)
-            append :pmcid, $1
+            append item, :pmcid, $1
           end
         end
       end
