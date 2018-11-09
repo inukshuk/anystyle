f272b029592cfc5e0139b065d0e684641bb51c08
Sylvester Keil <sylvester@keil.or.at>
2018-11-09 21:54:56 +0100

    Add DOI URL extraction samples
    See #116

--- spec/anystyle/normalizer/locator_spec.rb
+++ spec/anystyle/normalizer/locator_spec.rb
@@ -13,5 +13,14 @@ module AnyStyle
         expect(n.normalize(doi: [a])).to include(b)
       end
     end
+
+    it "extracts valid DOIs in URLs" do
+      ({
+        'https://doi.org/10/aabbe' => { doi: ['10/aabbe'] },
+        'https://doi.org/10.1000/182' => { doi: ['10.1000/182'] }
+      }).each do |(a, b)|
+        expect(n.normalize(url: [a])).to include(b)
+      end
+    end
   end
 end
