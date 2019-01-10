39f62a403a790a5cc7d4c4d8cc335e4d693f1b0f
Alex Fenton <fenton@dzhw.eu>
2019-01-10 15:26:12 +0100

    Normalisation of vol/issue for German language periodical citations
    Recognise Jahrgang/Heft. Add appropriate examples to the spec.
    Also make the spec report problems a bit more helpfully.

--- lib/anystyle/normalizer/volume.rb
+++ lib/anystyle/normalizer/volume.rb
@@ -3,35 +3,38 @@ module AnyStyle
     class Volume < Normalizer
       @keys = [:volume, :pages, :date]
 
+      VOLNUM_RX = '(\p{Lu}?\d+|[IVXLCDM]+)'
       def normalize(item, **opts)
         map_values(item, [:volume]) do |_, volume|
           volume = StringUtils.strip_html volume
 
           unless item.key?(:date)
-            unless volume.sub!(/([12]\d{3});|\(([12]\d{3})\)/, '').nil?
-              append item, :date, $1 || $2
+            unless volume.sub!(/([12]\d{3});|\(([12]\d{3})\)|\/([12]\d{3})/, '').nil?
+              append item, :date, $1 || $2 || $3
             end
           end
 
           case volume
-          when /(?:^|\s)(\p{Lu}?\d+|[IVXLCDM]+)\s?\(([^)]+)\)(\s?\d+\p{Pd}\d+)?/
+          when /(?:^|\s)#{VOLNUM_RX}\s?\(([^)]+)\)(\s?\d+\p{Pd}\d+)?/
             volume = $1
             append item, :issue, $2
             append item, :pages, $3.strip unless $3.nil?
             volume
-          when /(?:(\p{Lu}?\d+|[IVXLCDM]+)[\p{P}\s]+)?(?:nos?|nr|n°|nº|iss?|fasc)\.?\s?(.+)$/i
+          when /(?:#{VOLNUM_RX}
+                (?:\.?\s*J(?:ahrgan)?g\.?)?
+                [\p{P}\s]+)?(?:nos?|nr|n°|nº|iss?|fasc|heft|h)\.?\s?(.+)$/ix
             volume = $1
             append item, :issue, $2.sub(/\p{P}$/, '')
             volume
-          when /(\p{Lu}?\d+|[IVXLCDM]+):(\d+(\p{Pd}\d+)?)/
+          when /#{VOLNUM_RX}:(\d+(\p{Pd}\d+)?)/
             volume = $1
             append item, (($3.nil? || item.key?(:pages)) ? :issue : :pages), $2
             volume
-          when /(\p{Lu}?\d+|[IVXLCDM]+)[\.\/](\S+)/
+          when /#{VOLNUM_RX}[\.\/](\S+)/
             volume = $1
             append item, :issue, $2.sub(/\p{P}$/, '')
             volume
-          when /(\d+) [Vv]ol/
+          when /(\d+) [Vv]ol/, /J(?:ahrgan)?g\.? (\d+)/
             $1
           else
             volume
